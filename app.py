204dc28a5be3278ccd09e99bd80214fe330505d3
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 10:47:48 -0300

    inicializando a api pelo arquivo principal app.py

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {'message': 'Olá mundo!'}
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('app:app', host='127.0.0.1', port=8000, log_level='info', reload=True)
+    
