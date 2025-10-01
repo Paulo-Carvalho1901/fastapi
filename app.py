6d3429eea34b7ec00b301a49eaaa89a4baddd2ea
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 13:09:30 -0300

    mudando a host 0.0.0.0

--- app.py
+++ app.py
@@ -11,5 +11,4 @@ def root():
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('app:app', host='127.0.0.1', port=8000, log_level='info', reload=True)
-    
+    uvicorn.run('app:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
