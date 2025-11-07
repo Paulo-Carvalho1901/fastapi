155f871c5ac2f6a5753cf150e12eb0ac7560937b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-07 19:56:45 -0300

    test: creiado teste e conftest

--- test/test_app.py
+++ test/test_app.py
@@ -1,28 +1,9 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
-
-from app import app
-
-import pytest
-
-# Teste reutilizavel
-@pytest.fixture
-def client():
-    return TestClient(app)
 
 def test_root_deve_retornar_ola_mundo(client):
-    """
-    Esse teste tem 3 etapas (AAA)
-    - A: Arrange - Arranjo
-    - A: Act     - Executa a coisa (o SUT)
-    - A: Asert   - Garanta que A é A
-    """
-
-    # act
     response = client.get('/')
 
-    # Assert
     assert response.json() == {'message': 'Olá mundo!'}
     assert response.status_code == HTTPStatus.OK
 
