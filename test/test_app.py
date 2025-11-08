fdea74455196d58853cc3dcc853138d13eaa4b43
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-07 22:34:59 -0300

    test: (api) criado teste user_put

--- test/test_app.py
+++ test/test_app.py
@@ -39,3 +39,21 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+            '/users/1',
+            json={
+                'username': 'bob',
+                'email': 'bob@example.com',
+                'password': 'secret'
+            },
+        )
+    
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1
+    }
