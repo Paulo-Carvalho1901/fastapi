e4481b8ced465b8e4b4485c6b1bb78e2c51459c2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-07 21:18:48 -0300

    feat: (api) criado metodo put para atualizar usuario

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def root():
     return {'message': 'Olá mundo!'}
 
 
+# Criando metodos para inserir usuarios com (POST)
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPuplic)
 def create_user(user: UserSchema): # anotação de tipo, determinado pelo schema (entrada dos dados)
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1) # criando um desempacotamento dos dados users
@@ -21,10 +22,17 @@ def create_user(user: UserSchema): # anotação de tipo, determinado pelo schema
     return user_with_id
 
 
+# Criado metodo para ler todos os usuarios criados (GET)
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users(): 
     
     return {'users': database}
+
+
+# Criando metodo para atualizar dados do banco (PUT)
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPuplic)
+def update_user(user_id: int, user: UserSchema):
+    ...
 
 
 
