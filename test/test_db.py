09292c26d25a84c0782884dcac0011e7fec3c181
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-15 00:01:54 -0300

    test: session e commit no banco de dados

--- test/test_db.py
+++ test/test_db.py
@@ -1,11 +1,13 @@
 from models import User
 
 
-def test_create_user():
+def test_create_user(session):
     user = User(
         username='test',
         email='test@test',
         password='secret'
     )
+    session.add(user)
+    session.commit()
 
     assert user.username == 'test'
