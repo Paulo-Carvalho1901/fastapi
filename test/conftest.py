f6f9561f10efb2b4795914b60dfed1bfdd8040a9
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-14 23:57:07 -0300

    test: configurando conftest

--- test/conftest.py
+++ test/conftest.py
@@ -10,5 +10,17 @@ def client():
     return TestClient(app)
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import table_registry
+
+
+@pytest.fixure
 def session():
-    ...
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
