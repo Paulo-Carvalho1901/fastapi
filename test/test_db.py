269a45e901703a0b8d5dd2fc9be6c0bb031259bb
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-02-21 20:56:56 -0300

    Create teste user

--- test/test_db.py
+++ test/test_db.py
@@ -1,13 +1,19 @@
+from sqlalchemy import select
+
 from models import User
 
 
 def test_create_user(session):
-    user = User(
+    new_user = User(
         username='test',
         email='test@test',
         password='secret'
     )
-    session.add(user)
+    session.add(new_user)
     session.commit()
+
+    user = session.scalar(
+        select(User).where(User.username == 'test')
+    )
 
     assert user.username == 'test'
